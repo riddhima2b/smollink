62ffadc5a94edc73ef765d526bdb6efaeea8e0c1
RIDDHIMA BHALLA <riddhimabhalla@RIDDHIMAs-Air.bbrouter>
2026-06-23 21:34:07 +0530

    bg

--- frontend/ui/src/App.jsx
+++ frontend/ui/src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <>
       
-      <div className='bg-blue-500 text-white p-4 rounded-lg shadow-md'>
-        Counter = {count}
+      <div className='bg-blue-500 text-white p-4 rounded-lg shadow-md max-h-full h-screen'>
+        <div>
+          <h1 className='text-2xl font-bold'>Hello Vite + React!</h1>
+        </div>
 
       </div>
         
