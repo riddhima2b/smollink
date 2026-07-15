0770f460c1005c0c2e3f99f8d463fdcc555b9494
RIDDHIMA BHALLA <riddhimabhalla@RIDDHIMAs-Air.bbrouter>
2026-07-15 13:41:32 +0530

    service layer update

--- backend/src/services/link.service.js
+++ backend/src/services/link.service.js
@@ -1,12 +1,35 @@
-const encodeBase62 = require('../utils/base62');
+const Base62 = require('../utils/base62');
 const prismaclient = require('../lib/prisma');
+
+async function fetchUrl(url)
+{
+    const link = await prismaclient.primsa.link.findUnique({
+        where: { longUrl: url },
+    });
+
+    return link;
+}
 async function createShortUrl(url) {
 
-    await prismaclient.primsa.link.create({
-        data: {
-            url: url,
-            shortUrl: encodeBase62(),
-        },
+    const existingLink = await fetchUrl(url);
+    if(existingLink) {
+        return existingLink;
+    }
+    
+    const newLink = await prismaclient.primsa.link.create({
+            data: {
+                longUrl: url,
+            },
+        });
+        
+    const shortUrl = Base62.encodeBase62(newLink.id);
+
+    const shortLink = await prismaclient.primsa.link.update({
+        
+        where: { id: newLink.id },
+        data: { shortCode: shortUrl },
     });
+
+    return shortLink;
     
 }
