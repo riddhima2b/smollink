0463c9e96da2e8dd416a3291fcfb29e0bcef1268
RIDDHIMA BHALLA <riddhimabhalla@RIDDHIMAs-Air.bbrouter>
2026-08-01 14:39:29 +0530

    front end

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    purge: [],
+    darkMode: true, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
