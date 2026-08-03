ea86a3fcf99cd8724d4c32bfb88e12789a2842b0
RIDDHIMA BHALLA <riddhimabhalla@RIDDHIMAs-Air.bbrouter>
2026-08-03 12:45:18 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import LandingPage from "./pages/landingPage";
+function App() {
+
+  return (
+    <>
+    <Routes>
+      
+    <Route path="/" element={<LandingPage />} />
+    </Routes>    
+    </>
+  )
+}
+
+export default App
