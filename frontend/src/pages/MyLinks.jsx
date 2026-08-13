a8ace733cce4169158b43f32e573404e86175465
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-08-13 14:53:55 +0530

    links

--- frontend/src/pages/MyLinks.jsx
+++ frontend/src/pages/MyLinks.jsx
@@ -1,21 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-
+import Toast from "../components/Toast";
 const MyLinks = () => {
 
-    const fetchLinks = () =>{
+    const [toast, setToast] = useState(null);
+    const [link, setlinks] = useState([]);
 
-        const userLinks = [];
+    useEffect(() => {
 
-        
-    }
+        const fetchLinks = async() =>{
+    
+            try{
+                const links = await axios.get('http://localhost:3001/api/mylinks', 
+                { withCredentials: true });
+                const data = links.data;
+                console.log("API data:", data);
+                setlinks(data);
+            }
+            catch(error){
+                console.error(error);
+                setToast(error.response?.data?.error || "Something went wrong. Please try again.");
+            }
+
+        }
+        fetchLinks();
+}, []);
 
     return (
         <div>
+        <Toast message={toast} onClose={() => setToast(null)} />
             <div className="relative min-h-screen overflow-hidden bg-radial bg-[#0B0A12] text-white font-serif brightness-100 opacity-95 mb-10">
                 <Navbar />
-
-                <h1>My Links</h1>
-                <p>This is the My Links page.</p>
+                
+                <h1 className="flex flex-col items-center-safe text-4xl text-[#ff4f87] p-3">My Links</h1>
+                <div className="text-white text-lg"><pre>{JSON.stringify(link, null, 2)}</pre></div>
             </div>
         </div>
     );
