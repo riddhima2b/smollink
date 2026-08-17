acc0554738523f421e8f06ecb8949777b154899e
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-08-17 18:30:51 +0530

    update urls

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
         const { token, user: safeUser } = await loginUser(email, password);
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production' && false,
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'lax',
             expires: new Date(Date.now() + 60 * 60 * 1000)
         });        return res.status(200).json({ user: safeUser });        
